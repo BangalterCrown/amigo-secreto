b5bc7d762f8c1a7393d0a3ab77d9b2195f68ccb3
BangalterCrown <husseinpedraza@icloud.com>
2025-02-05 02:30:47 -0600

    Agregando png al index

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function nombresTextoElementos(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+
+function ingresaTuNombre() {
+    let nombreDeUsuario = document.getElementById('amigo').value
+    console.log(nombreDeUsuario);
+    return;
+}
+
+nombresTextoElementos('h1','Amigo Secreto');
+nombresTextoElementos('h2','Ingresa el nombre de tus amigos');
+nombresTextoElementos('h1','Amigo Secreto')
