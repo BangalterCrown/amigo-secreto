1e2dc9df24fa3dce8e6578b48cfd3413a98793c8
BangalterCrown <husseinpedraza@icloud.com>
2025-02-08 23:46:54 -0600

    Creando Array Amigos y Tareas especificas

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Instrucciones del curso, paso 1: Crea una lista de nombre "Amigos" 
+
 let amigos = [];
 
+//Desarrolla una función, que permita al usuario ingresar un nombre en el campo de texto 
+// y añadirlo a la lista de amigos creada anteriormente.
+//Tareas especificas 
 
-function nombresTextoElementos(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
+function agregarAmigo(elemento, texto) {
+    let elementoHTML = document.getElementById('amigoAgregado');
     elementoHTML.innerHTML = texto;
     return;
 }
 
-let nombreAmigo = document.getElementById("amigo");
-let listadoAmigos = document.getElementById("listaAmigos");
-let listaResultados= document.getElementById("resultado");
-
-function ingresaAmigo() {
-    let nombre = nombreAmigo.value.trim();
-    if(nombre === "") {
-        alert("Por favor, ingresa un nombre.");
-        return;
-    }
-
-    if (!amigos.includes(nombre)) {
-        amigos.push(nombre);
-        actualizarListado();
-    } else {
-        alerta("Nombre duplicado, ingresa uno válido")
-    }
-    nombreAmigo.value = "";
-}
-
-function actualizarListado() {
-
-}
 
 
-nombresTextoElementos('h1','Amigo Secreto');
+nombresTextoElementos('h1','Bienvenido al Juego del Amigo Secreto');
 nombresTextoElementos('h2','Ingresa el nombre de tus amigos');
